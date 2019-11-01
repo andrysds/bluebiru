297ddb569161980aba1f267e6364dc2e71e37af8
Andrys Daniel Silalahi <andrysds@gmail.com>
2019-11-01 12:56:01 +0700

    add http server and bot example

--- main.go
+++ main.go
@@ -1,35 +1,17 @@
 package main
 
 import (
+	"io"
 	"log"
+	"net/http"
 	"os"
-
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
+func healthz(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "OK!")
+}
+
 func main() {
-	bot, err := tgbotapi.NewBotAPI(os.Getenv("BOT_TOKEN"))
-	if err != nil {
-		log.Panic(err)
-	}
-
-	log.Printf("Authorized on account %s", bot.Self.UserName)
-
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
-
-	updates, err := bot.GetUpdatesChan(u)
-
-	for update := range updates {
-		if update.Message == nil { // ignore any non-Message Updates
-			continue
-		}
-
-		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-		msg.ReplyToMessageID = update.Message.MessageID
-
-		bot.Send(msg)
-	}
+	http.HandleFunc("/healthz", healthz)
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), nil))
 }
